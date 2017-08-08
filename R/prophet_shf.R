0996e091d91f985d716f686fac995523344f123b
hoxo-m <hoxo.smile@gmail.com>
2017-08-08 21:26:37 +0900

    fix shf

--- R/prophet_shf.R
+++ R/prophet_shf.R
@@ -1,26 +1,30 @@
 #' Simulated Historical Forecast
 #'
 #' @param model Prophet model object.
-#' @param periods periods
-#' @param freq freq
-#' @param k k
-#' @param overlap overlap
+#' @param periods Integer.
+#' @param k Integer.
+#' @param overlap Numeric.
+#' @param measure_func Function or character.
+#'
+#' @return A prophet shf object.
 #'
 #' @import prophet
 #' @importFrom stats loess predict
-#' @importFrom utils head tail
 #' @export
-prophet_shf <- function(model, periods, freq = "d", k = 3, overlap = 0.5) {
-  N <- nrow(model$history)
-  denom <- round(1 / (1 - overlap))
-  overlap <- 1 / denom
-  periods <- periods + periods %% denom  # periods to odd number
-  preserve <- (k - 1) * periods / denom + periods
-  n_history <- N - preserve - 1
-  if (n_history < periods * 2) warning("History is too short.")
-  result <- data.frame()
-  while (n_history < N - periods) {
-    data_hist <- head(model$history, n_history)
+prophet_shf <- function(model, periods, k = 3, overlap = 0.5, measure_func = c("MAPE", "MAE")) {
+  if (is.character(measure_func)) {
+    measure_func <- match.arg(measure_func)
+    measure_func <- switch(measure_func,
+                           "MAPE" = function(act, pred) abs(act - pred) / act,
+                           "MAE" = function(act, pred) abs(act - pred))
+  }
+
+  data_hist <- model$history
+  boundary_ds <- get_boundary_ds(data_hist$ds, periods, k, overlap)
+
+  observed <- mapply(function(ds_bound, iter) {
+    df_train <- filter(data_hist, ds <= ds_bound)
+    df_test <- filter(data_hist, between(ds, ds_bound + 1, ds_bound + periods))
     m <- prophet(data_hist, growth = model$growth,
                  n.changepoints = model$n.changepoints,
                  yearly.seasonality = model$yearly.seasonality,
@@ -32,17 +36,27 @@ prophet_shf <- function(model, periods, freq = "d", k = 3, overlap = 0.5) {
                  mcmc.samples = model$mcmc.samples,
                  interval.width = model$interval.width,
                  uncertainty.samples = model$uncertainty.samples)
-    future <- make_future_dataframe(m, periods, freq = freq)
-    forecast <- predict(m, future)
-    tmp <- merge(forecast, model$history, by="ds")
-    pred <- tail(tmp$yhat, periods)
-    act <- tail(tmp$y, periods)
-    ape <- abs((act - pred) / act)
-    df <- data.frame(x = 1:periods, ape = ape)
-    result <- rbind(result, df)
-    n_history <- n_history + periods / denom
-  }
-  # print(ggplot(result, aes_string(x=x, y="ape"))+ geom_point() + geom_smooth(method = "loess", se=FALSE))
-  loess <- loess(ape ~ x, data = result)
-  predict(loess, data.frame(x = 1:periods))
+    fore <- predict(m, df_test)
+    x <- as.integer(fore$ds - ds_bound)
+    act <- fore$y
+    pred <- fore$yhat
+    value <- measure_func(act, pred)
+    data.frame(iter, x, value)
+  }, boundary_ds, seq_len(k), SIMPLIFY = FALSE)
+  observed <- Reduce(rbind, observed)
+
+  fitted_model <- loess(value ~ x, data = observed)
+  x <- seq_len(periods)
+  estimated <- data.frame(x, value=predict(fitted_model, x))
+  result <- list(estimated = estimated, observed = observed, fitted_model = fitted_model)
+  class(result) <- "prophet_shf"
+  result
+}
+
+get_boundary_ds <- function(ds, periods, k, overlap) {
+  interval <- min(diff(ds))
+  latest_ds <- max(ds)
+  boundary_ds <- latest_ds - periods * interval
+  boundary_ds <- c(boundary_ds, boundary_ds - ceiling(periods * interval * (1 - overlap)) * seq_len(k-1))
+  rev(boundary_ds)
 }
