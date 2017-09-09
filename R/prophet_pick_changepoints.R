3ae9054d347801eee1fbe0c8bbcc0d5d03206e7c
hoxo-m <hoxo.smile@gmail.com>
2017-09-09 17:58:41 +0900

    modify prophet_pick_changepoints()
    simpler significant changepoint

--- R/prophet_pick_changepoints.R
+++ R/prophet_pick_changepoints.R
@@ -12,29 +12,10 @@
 #' }
 #'
 #' @export
-prophet_pick_changepoints <- function(model, digits = 2) {
-  df <- data.frame(cp = model$changepoints,
-                   cp.t = model$changepoints.t,
-                   delta = as.vector(model$params$delta))
-  while(nrow(df) > 1 && any(abs(df$delta) < 10^-digits)) {
-    ind <- which.min(abs(df$delta))
-    if (ind == 1) {
-      pos <- 2
-    } else if (ind == nrow(df) || 2 * df$cp.t[ind] < df$cp.t[ind-1] + df$cp.t[ind+1]) {
-      pos <- ind - 1
-    } else {
-      pos <- ind + 1
-    }
-    df$delta[pos] <- df$delta[pos] + df$delta[ind]
-    df <- df[-ind, , drop = FALSE]
-  }
-  if (nrow(df) == 1 && abs(df$delta) < 10^-digits) {
-    df <- data.frame()
-  }
-  cp <- c(model$start, df$cp)
-  delta <- c(0, df$delta)
-  growth_rate <- model$params$k + cumsum(delta)
-  result <- data.frame(changepoints = cp, growth_rate = growth_rate, delta = delta)
-  class(result) <- c("prophet_changepoint", class(result))
-  result
+prophet_pick_changepoints <- function(model, thresh = 0.01) {
+  cp_index <- c(1, which(abs(model$params$delta) >= thresh) + 1)
+  changepoints <- c(model$start, model$changepoints)[cp_index]
+  growth_rate <- model$params$k + c(0, cumsum(model$params$delta))[cp_index]
+  delta <- c(NA_real_, model$params$delta)[cp_index]
+  data.frame(changepoint = changepoints, growth_rate = growth_rate, delta = delta)
 }
